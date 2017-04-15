32bdffbefa271de6125a356fae2cf653b3c2690d
Ahmed Khaled <ahmedkhaled1123300@gmail.com>
2017-04-15 20:44:38 +0200

    added simple driver header file

---
+++ assembler/AssemblerDirective.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "AssemblerDirective.h"
+
+using namespace std;
+
+const int COLUMNS_SIZE = 3;
+
+AssemblerDirective::AssemblerDirective()
+{
+    //TODO.
+}
---
+++ assembler/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "AssemblerDirective.h"
+
+using namespace std;
+
+int main(void)
+{
+
+    return 0;
+}
