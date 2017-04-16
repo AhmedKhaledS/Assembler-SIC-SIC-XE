f866de2f5646835ec77c266302b019665ca89761
Ahmed Naggar <ahmednagar2011@gmail.com>
2017-04-16 22:59:59 +0200

    Minor Changes in txt files

--- assembler/tables/OperationCodeTable.h
+++ assembler/tables/OperationCodeTable.h
@@ -1,23 +1,20 @@
-#ifndef OPERATIONCODETABLE_H
-#define OPERATIONCODETABLE_H
 #include <map>
 #include <iostream>
 #include <stdio.h>
 
+using namespace std;
+
 class OperationCodeTable {
     private :
 
-    static std::map<std::string, std::string> opCodeTable;
+    static map<string, string> opCodeTable;
 
     public :
 
     static void load();
 
-    static bool searchOperation(std::string operation);
+    static bool searchOperation(string operation);
 
-    static std::string getCode(std::string operation);
+    static string getCode(string operation);
 };
 
-
-
-#endif // OPERATIONCODETABLE_H
